1756feca4bdba71f813640b7366eb69b29c0eb5c
Oliver Forral <intrepion@gmail.com>
2022-10-03 14:02:01 -0600

    red

--- greeting/json-rpc/full-stack/typescript-react-rust-actix-web/start_local_server.sh
+++ greeting/json-rpc/full-stack/typescript-react-rust-actix-web/start_local_server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./greeting/json-rpc/server/rust-actix-web/start_local.sh "http://localhost:3000"
+./greeting/json-rpc/server/rust-actix-web/start_local_server.sh "http://localhost:3000"
--- greeting/json-rpc/tests/cypress/cypress/e2e/greeting.cy.js
+++ greeting/json-rpc/tests/cypress/cypress/e2e/greeting.cy.js
@@ -1,5 +1,9 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
-  });
-});
+describe('greeting app', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('should have hello world message by default', () => {
+    cy.get('p.greeting').should('have.text', 'Hello, World!')
+  })
+})
