07bbafefa0e6ad3b4a4a2069a139abbd43e73abc
Oliver Forral <intrepion@gmail.com>
2022-10-03 16:44:48 -0600

    red

--- greeting/json-rpc/tests/cypress/cypress/e2e/greeting.cy.js
+++ greeting/json-rpc/tests/cypress/cypress/e2e/greeting.cy.js
@@ -1,9 +1,16 @@
-describe('greeting app', () => {
+describe("greeting app", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
-  })
+    cy.visit("http://localhost:3000");
+  });
 
-  it('should have hello world message by default', () => {
-    cy.get('p.greeting').should('have.text', 'Hello, World!')
-  })
-})
+  it("should have hello world message by default", () => {
+    cy.get("p.greeting").should("have.text", "Hello, World!");
+  });
+
+  it("should have hello alice message when Alice is typed", () => {
+    cy.get("input.name").type("Alice");
+    cy.get("button.submit").click();
+    cy.get("p.greeting").should("have.text", "Hello, Alice!");
+    cy.get("@post").should("have.property", "status", 200);
+  });
+});
