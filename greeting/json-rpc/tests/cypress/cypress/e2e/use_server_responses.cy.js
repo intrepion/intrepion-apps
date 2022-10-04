8ad1de193d9566682e69459bc80abfce7e0d89f8
Oliver Forral <intrepion@gmail.com>
2022-10-04 13:16:46 -0600

    npx prettier --write .

--- greeting/json-rpc/tests/cypress/cypress/e2e/use_server_responses.cy.js
+++ greeting/json-rpc/tests/cypress/cypress/e2e/use_server_responses.cy.js
@@ -8,11 +8,11 @@ describe("greeting app", () => {
   });
 
   it("should have hello alice message when Alice is typed", () => {
-    cy.intercept('POST', '/api').as('api')
+    cy.intercept("POST", "/api").as("api");
     cy.get("input.name").type("Alice");
     cy.get("button.submit").click();
 
-    cy.wait('@api')
+    cy.wait("@api");
 
     cy.get("p.greeting").should("have.text", "Hello, Alice!");
   });
