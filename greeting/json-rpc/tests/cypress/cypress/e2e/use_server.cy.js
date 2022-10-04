2bf5b3779c5e58d078fe66109564e6e0cb797956
Oliver Forral <intrepion@gmail.com>
2022-10-03 20:51:35 -0600

    Using real server responses.

--- greeting/json-rpc/tests/cypress/cypress/e2e/use_server.cy.js
+++ greeting/json-rpc/tests/cypress/cypress/e2e/use_server.cy.js
@@ -8,9 +8,12 @@ describe("greeting app", () => {
   });
 
   it("should have hello alice message when Alice is typed", () => {
+    cy.intercept('POST', '**/api').as('api')
     cy.get("input.name").type("Alice");
     cy.get("button.submit").click();
+
+    cy.wait('@api')
+
     cy.get("p.greeting").should("have.text", "Hello, Alice!");
-    cy.get("@post").should("have.property", "status", 200);
   });
 });
