2edff010b8ac43eee4db99db5bc3dacc47ff0718
Oliver Forral <intrepion@gmail.com>
2022-11-30 18:34:08 -0700

    Adjusted script to be more normalized.

--- greeting/json-rpc/full-stack/typescript-react-rust-actix-web/start_local_client.sh
+++ greeting/json-rpc/full-stack/typescript-react-rust-actix-web/start_local_client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./greeting/json-rpc/client/typescript-react/start_local_client.sh "http://localhost:8080/api"
+./greeting/json-rpc/client/typescript-react/start_local_client.sh "http://localhost:8080"
