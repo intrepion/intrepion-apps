75baf6156c5375f58e7e344619f8bb186d4e1fa9
Oliver Forral <intrepion@gmail.com>
2023-01-01 11:54:00 -0700

    Add run scripts.

--- apps/saying-hello/json-rpc/full_stack/typescript-react-csharp-dotnet/start_full_stack-typescript-webapi_client.sh
+++ apps/saying-hello/json-rpc/full_stack/typescript-react-csharp-dotnet/start_full_stack-typescript-webapi_client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./apps/saying-hello/json-rpc/client-web/start_client-web-typescript-react-typescript.sh "http://localhost:80"
+./apps/saying-hello/json-rpc/client-web/start_client-web-typescript-react-typescript.sh ""
