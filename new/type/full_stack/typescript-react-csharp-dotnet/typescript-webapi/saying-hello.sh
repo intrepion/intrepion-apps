0b3ffa365c2f02942c86d1069a47db8eb9003300
Oliver Forral <intrepion@gmail.com>
2023-01-02 16:51:43 -0700

    Fixed bugs.

--- new/type/full_stack/typescript-react-csharp-dotnet/typescript-webapi/saying-hello.sh
+++ new/type/full_stack/typescript-react-csharp-dotnet/typescript-webapi/saying-hello.sh
@@ -582,33 +582,66 @@ git add $FILE
 FILE=src/components/SayingHello.tsx
 
 cat > $FILE << EOF
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Greeting from "./Greeting";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL ?? "http://localhost:3000";
 
+type GreetingType = {
+  id: string;
+  message: string;
+  name: string;
+};
+
 const SayingHello: React.FC = () => {
+  const [greetings, setGreetings] = useState<GreetingType[]>([]);
   const [name, setName] = useState("");
   const [message, setMessage] = useState("Hello, world!");
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
-  };
-
-  const callEndpoint = () => {
+  useEffect(() => {
     fetch(SERVER_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         id: "1",
         jsonrpc: "2.0",
-        method: "say_hello",
+        method: "get_all_greetings",
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.result) {
+          const result = responseJson.result;
+          setGreetings(result.greetings);
+        } else if (responseJson.error) {
+          console.error(responseJson.error);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [message]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const callSayHello = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    fetch(SERVER_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: "1",
+        jsonrpc: "2.0",
+        method: "new_greeting",
         params: { name },
       }),
     })
       .then((response) => response.json())
       .then((responseJson) => {
         const result = responseJson.result;
-        setMessage(result.saying);
+        setMessage(result.message);
       })
       .catch((error) => {
         console.error(error);
@@ -618,8 +651,20 @@ const SayingHello: React.FC = () => {
   return (
     <div>
       <input type="text" value={name} onChange={handleChange} />
-      <button onClick={callEndpoint}>Call Endpoint</button>
+      <button onClick={callSayHello}>Say Hello</button>
       <div>{message}</div>
+      <p>Previous Greetings</p>
+      <ul>
+        {greetings.map((greeting) => (
+          <li key={greeting.id}>
+            <Greeting
+              id={greeting.id}
+              message={greeting.message}
+              name={greeting.name}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
@@ -628,6 +673,30 @@ export default SayingHello;
 EOF
 
 git add $FILE
+
+FILE=src/components/Greeting.tsx
+
+cat > $FILE << EOF
+import React from "react";
+
+interface GreetingInterface {
+  id: string;
+  message: string;
+  name: string;
+}
+
+const Greeting = (props: GreetingInterface) => {
+  const { id, name, message } = props;
+
+  return (
+    <>
+      {id}: {name}, {message}
+    </>
+  );
+};
+
+export default Greeting;
+EOF
 
 npx prettier --write .
 git commit --message "Added saying hello form."
