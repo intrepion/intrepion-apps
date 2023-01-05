eddf457232765098850af5ec5e91adda345efc74
Oliver Forral <intrepion@gmail.com>
2023-01-05 02:11:21 -0700

    Add run scripts.

--- apps/to-do/json-rpc/full_stack/typescript-react-csharp-dotnet/start_full_stack-typescript-webapi_client.sh
+++ apps/to-do/json-rpc/full_stack/typescript-react-csharp-dotnet/start_full_stack-typescript-webapi_client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./apps/to-do/json-rpc/client-web/start_client-web-typescript-react-typescript.sh "http://localhost:5014"
+./apps/to-do/json-rpc/client-web/start_client-web-typescript-react-typescript.sh "http://localhost:5093"
