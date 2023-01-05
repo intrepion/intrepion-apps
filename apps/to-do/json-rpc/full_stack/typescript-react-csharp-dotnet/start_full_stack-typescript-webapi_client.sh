d4ee0e855eedec07625f893623eebe30317726d7
Oliver Forral <intrepion@gmail.com>
2023-01-04 18:06:51 -0700

    Add run scripts.

---
+++ apps/to-do/json-rpc/full_stack/typescript-react-csharp-dotnet/start_full_stack-typescript-webapi_client.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./apps/to-do/json-rpc/client-web/start_client-web-typescript-react-typescript.sh "http://localhost:5265"
---
+++ apps/to-do/json-rpc/server/start_server-csharp-dotnet-webapi.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+CLIENT_URL=$1 dotnet run --project ../intrepion-to-do-json-rpc-server-csharp-dotnet-webapi/ToDoWebApi
