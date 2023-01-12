419ac4b3718e2baf5eb2e360d55233aa04f87bbf
Oliver Forral <intrepion@gmail.com>
2023-01-11 20:38:04 -0700

    Adding front end for quiz script.

--- new/common/checkout_first_commit.sh
+++ new/common/checkout_first_commit.sh
@@ -7,20 +7,15 @@ echo " - Running $SCRIPT $REPOSITORY"
 
 pushd .
 
-# Check if the specified folder exists
 if [ ! -d "$REPOSITORY" ]; then
-  # If the folder does not exist, clone the repository using SSH
   git clone git@github.com:intrepion/$REPOSITORY.git
 fi
 
-# Navigate to the repository folder
 cd $REPOSITORY
 pwd
 
-# Checkout main branch
 git checkout main
 
-# Reset to first commit
 FIRST=`git rev-list --max-parents=0 HEAD`
 git reset --hard $FIRST
 git clean -d --force
--- new/type/full_stack/session-json-rpc/typescript-react-csharp-dotnet/typescript-webapi/quiz.sh
+++ new/type/full_stack/session-json-rpc/typescript-react-csharp-dotnet/typescript-webapi/quiz.sh
@@ -503,3 +503,59 @@ dotnet test && git commit --message="refactor - using fluent assertions to check
 git push --force
 
 cd ..
+
+if [ ! -d "$CLIENT_REPOSITORY" ]; then
+  git clone git@github.com:intrepion/$CLIENT_REPOSITORY.git
+fi
+
+cd $CLIENT_REPOSITORY
+pwd
+
+git checkout main
+
+FIRST=`git rev-list --max-parents=0 HEAD`
+git reset --hard $FIRST
+git clean -d --force
+
+cd $CLIENT_REPOSITORY
+pwd
+
+npx create-react-app . --template $CLIENT_TEMPLATE
+git add --all
+git commit --message "npx create-react-app . --template $CLIENT_TEMPLATE"
+
+mv README.old.md README.md
+git add README.old.md README.md
+git commit --message "mv README.old.md README.md"
+
+npm install --save-dev --save-exact prettier
+git add --all
+git commit --message "npm install --save-dev --save-exact prettier"
+
+echo {}> .prettierrc.json
+git add .prettierrc.json
+git commit --message "echo {}> .prettierrc.json"
+
+cp .gitignore .prettierignore
+git add .prettierignore
+git commit --message "cp .gitignore .prettierignore"
+
+npx prettier --write .
+git add --all
+git commit --message "npx prettier --write ."
+
+npm install uuid
+git add --all
+git commit --message "npm install uuid"
+
+npm i --save-dev @types/uuid
+git add --all
+git commit --message "npm i --save-dev @types/uuid"
+
+npx prettier --write .
+git add --all
+git commit --message "npx prettier --write ."
+
+git push --force
+
+cd ..
